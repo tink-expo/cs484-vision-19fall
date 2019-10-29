4d6ef506a46916c7b01121747d916fccf711eae2
tink-expo <whhan8485@gmail.com>
2019-10-29 14:05:43 +0900

    hw3 initial commit

---
+++ assignment_3/hw3/code/bayer_to_rgb_bicubic.m
@@ -0,0 +1,15 @@
+%% HW3-a
+% Generate the rgb image from the bayer pattern image using linear and
+% bicubic interpolation.
+function rgb_img = bayer_to_rgb_bicubic(bayer_img)
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Your code here
+    disp(size(bayer_img))
+
+    %% Delete here when you run your code
+    rgb_img = bayer_img;
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+end
