bea593d72e5d50da7db449f368063310c09d061a
tink-expo <whhan8485@gmail.com>
2019-11-29 17:06:22 +0900

    hw4-submit first commit

---
+++ assignment_4/hw4-submit/code/get_descriptors.m
@@ -0,0 +1,137 @@
+% Local Feature Stencil Code
+% Written by James Hays for CS 143 @ Brown / CS 4476/6476 @ Georgia Tech
+
+% Returns a set of feature descriptors for a given set of interest points. 
+
+% 'image' can be grayscale or color, your choice.
+% 'x' and 'y' are nx1 vectors of x and y coordinates of interest points.
+%   The local features should be centered at x and y.
+% 'descriptor_window_image_width', in pixels, is the local feature descriptor width. 
+%   You can assume that descriptor_window_image_width will be a multiple of 4 
+%   (i.e., every cell of your local SIFT-like feature will have an integer width and height).
+% If you want to detect and describe features at multiple scales or
+% particular orientations, then you can add input arguments.
+
+% 'features' is the array of computed features. It should have the
+%   following size: [length(x) x feature dimensionality] (e.g. 128 for
+%   standard SIFT)
+
+function [features] = get_features(image, x, y, scale_indices, descriptor_window_image_width, scales)
+
+% To start with, you might want to simply use normalized patches as your
+% local feature. This is very simple to code and works OK. However, to get
+% full credit you will need to implement the more effective SIFT descriptor
+% (See Szeliski 4.1.2 or the original publications at
+% http://www.cs.ubc.ca/~lowe/keypoints/)
+
+% Your implementation does not need to exactly match the SIFT reference.
+% Here are the key properties your (baseline) descriptor should have:
+%  (1) a 4x4 grid of cells, each descriptor_window_image_width/4. 'cell' in this context
+%    nothing to do with the Matlab data structue of cell(). It is simply
+%    the terminology used in the feature literature to describe the spatial
+%    bins where gradient distributions will be described.
+%  (2) each cell should have a histogram of the local distribution of
+%    gradients in 8 orientations. Appending these histograms together will
+%    give you 4x4 x 8 = 128 dimensions.
+%  (3) Each feature should be normalized to unit length
+%
+% You do not need to perform the interpolation in which each gradient
+% measurement contributes to multiple orientation bins in multiple cells
+% As described in Szeliski, a single gradient measurement creates a
+% weighted contribution to the 4 nearest cells and the 2 nearest
+% orientation bins within each cell, for 8 total contributions. This type
+% of interpolation probably will help, though.
+
+% You do not have to explicitly compute the gradient orientation at each
+% pixel (although you are free to do so). You can instead filter with
+% oriented filters (e.g. a filter that responds to edges with a specific
+% orientation). All of your SIFT-like feature can be constructed entirely
+% from filtering fairly quickly in this way.
+
+% You do not need to do the normalize -> threshold -> normalize again
+% operation as detailed in Szeliski and the SIFT paper. It can help, though.
+
+% Another simple trick which can help is to raise each element of the final
+% feature vector to some power that is less than one.
+
+if mod(descriptor_window_image_width, 4) ~= 0
+    return
+end
+
+[img_h, img_w] = size(image);
+
+k = size(x, 1);
+features = zeros(k, 128);
+
+cell_width = descriptor_window_image_width / 4;
+gauss_windows = zeros([descriptor_window_image_width, descriptor_window_image_width, size(scales, 2)]);
+for i = 1 : size(scales, 2)
+    gauss_windows(:, :, i) = ...
+        fspecial('gaussian', descriptor_window_image_width, ...
+        scales(i) * 1.5);
+end
+
+for pt_idx = 1 : k
+    pt_pos = [y(pt_idx), x(pt_idx)];
+    
+    window_top = pt_pos - descriptor_window_image_width / 2;
+    window_bottom = window_top + descriptor_window_image_width - 1;
+    window_image = image(...
+        window_top(1):window_bottom(1), window_top(2):window_bottom(2));
+    [dirs, mags] = get_gradient(window_image);
+    dirs = min(max(ceil((dirs + 180) / 45), 1), 8);
+    for cell_y = 1 : 4
+        for cell_x = 1 : 4
+            cell_top = cell_width * [cell_y - 1, cell_x - 1] + 1;
+            cell_bottom = cell_top + cell_width - 1;
+            
+            dirs_cell = dirs( ...
+                cell_top(1):cell_bottom(1), ...
+                cell_top(2):cell_bottom(2));
+            dirs_cell = reshape(dirs_cell, 1, numel(dirs_cell));
+            
+            mags_cell = mags( ...
+                cell_top(1):cell_bottom(1), ...
+                cell_top(2):cell_bottom(2));
+            mags_cell = mags_cell .* gauss_windows( ...
+                cell_top(1):cell_bottom(1), ...
+                cell_top(2):cell_bottom(2), ...
+                scale_indices(pt_idx));
+            mags_cell = reshape(mags_cell, 1, numel(mags_cell));
+
+            cell_idx = 4 * (cell_y - 1) + cell_x;
+            feature_cell_start = (cell_idx - 1) * 8;
+            for i = 1 : numel(dirs_cell)
+                features(pt_idx, feature_cell_start + dirs_cell(i)) = ...
+                    features(pt_idx, feature_cell_start + dirs_cell(i)) ...
+                    + mags_cell(i);
+            end
+        end
+    end
+end
+features = features .^ 0.7;
+features = normalize(features', 'norm', 1)';
+features = min(features, 0.2);
+features = normalize(features', 'norm', 1)';
+
+end
+
+function [direction, magnitude] = get_gradient(img)
+
+h = -fspecial('prewitt');
+grad_x = imfilter(img, h', 'replicate');
+grad_y = imfilter(img, h, 'replicate');
+
+magnitude = hypot(grad_x, grad_y);
+direction = atan2(-grad_y, grad_x) * 180 / pi;
+
+end
+
+
+
+
+
+
+
+
+
